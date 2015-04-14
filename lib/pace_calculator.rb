8f1636d11b3fa0f531cec0e30151000c899fc8ec
Ann Wallace <ann.wallace@nike.com>
2015-04-14 09:26:26 -0700

    added hash to mile_and_time:x

--- lib/pace_calculator.rb
+++ lib/pace_calculator.rb
@@ -11,6 +11,8 @@
 # @author ann wallace
 #
 
+require 'pp'
+
 class PaceCalculator
 
    attr_accessor :distance, :finish_time, :start_time
@@ -42,6 +44,7 @@ class PaceCalculator
 		hour = get_hour(@start_time)
  		minutes = get_min(@start_time)
  		seconds = 0
+ 		miles_time = Hash.new 
 
 		@distance.to_i.times do |i|
 			# format correcly
@@ -49,13 +52,14 @@ class PaceCalculator
 			formatted_secs = '%02d' % seconds
 	  		time = "#{hour}:#{formatted_mins}:#{formatted_secs}"
 	  		puts "mile #{i} : time #{time}"
+	  		miles_time[i] = time
 	  		minutes += pace_min.to_i
 	  		if minutes >= 60 
 	  			minutes -= 60
 	  			hour += 1
 	  		end
-	  		# format correcly
 		end
+		pp miles_time
 	end
 
 	def get_hour(time)
